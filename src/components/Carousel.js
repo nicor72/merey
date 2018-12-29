b4a969297ed7174e08408c929950da89f18d3e43
Nicolas Roman <nicolas.roman@nidarbox.com>
2018-12-28 22:45:45 -0300

    feat(Carousel): add react-slick

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
-import logo from './../images/merey_logo.png';
+import Slider from "react-slick";
+
+import mani from './../images/mani.jpg';
+import tiendaMerey from './../images/tienda_merey.jpg';
+import dulces from './../images/dulces.jpg';
+import work from './../images/work.jpg';
 
 class Carousel extends Component {
   render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      fade: true,
+      pauseOnHover: false,
+      accessibility: true
+    };
     return (
-      <div>
-        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={logo} className="d-block w-100" alt="logo" />
-            </div>
-            <div className="carousel-item">
-              <img src={logo} className="d-block w-100" alt="logo" />
-            </div>
-            <div className="carousel-item">
-              <img src={logo} className="d-block w-100" alt="logo" />
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
+      <Slider {...settings}>
+        <img src={dulces} />
+        <img src={work} />
+      </Slider>
     );
   }
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ class Logo extends Component {
   render() {
     return (
       <div>
-        <div className="row justify-content-md-center">
+        <div className="row justify-content-md-center bg-black nav-height">
           <div className="col-md-auto">
             <img src={logo} className="App-logo" alt="logo" />
           </div>
