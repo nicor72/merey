2a202230bab4d5c64172a5ab8cb96951faffc265
nico <nroman@eclass.cl>
2020-08-22 18:41:24 -0400

    feat(navbar): sidebar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,46 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Badge } from 'react-bootstrap'
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+import SideNav, { MenuIcon } from 'react-simple-sidenav'
 import styled from 'styled-components'
-
 import Logo from './Logo'
+import Search from './Search'
 
 const NavStyle = styled.div`
-  nav {
+  background-color: black !important;
+  padding: 2rem;
+
+  .navbar {
     background-color: black !important;
-    padding: 0 1rem;
   }
+
+  ul {
+    list-style-type: none;
+  }
+
+  li {
+    padding: 0 !important;
+    a {
+      color: black;
+    }
+  }
+  
   .badge {
     margin-left: .5rem;
+  }
+
+  @media (min-width: 992px) {
+    svg {
+      display: none;
+    }
   }
 `
 
 export default () => {
   const ref = useRef(null)
+  const [showNav, setShowNav] = useState()
   const { cart } = useSelector((state) => state)
 
   window.onscroll = () => {
@@ -30,9 +52,54 @@ export default () => {
     }
   }
 
+  const navItems = [
+    <Search setShowNav={setShowNav}/>,
+    <Link to="/empresa" className="nav-link" onClick={() => setShowNav(false)}>Empresa</Link>,
+    <Link to="/productos" className="nav-link" onClick={() => setShowNav(false)}>Productos</Link>,
+    <Link to="/estamos" className="nav-link" onClick={() => setShowNav(false)}>Estamos</Link>,
+    <Link to="/contacto" className="nav-link" onClick={() => setShowNav(false)}>Contacto</Link>,
+    <Link to="/carrito" className="nav-link" onClick={() => setShowNav(false)}>
+      Carrito
+      {
+        cart.length > 0 &&
+        <Badge variant="light">{cart.length}</Badge>
+      }
+    </Link>
+  ];
+
   return (
     <NavStyle>
-      <Navbar ref={ref} bg="dark" variant="dark" expand="lg">
+      <Navbar ref={ref} variant="dark" expand="lg">
+        <Link to="/" className="navbar-brand">
+          <Logo />
+        </Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/empresa" className="nav-link">Empresa</Link>
+            <Link to="/productos" className="nav-link">Productos</Link>
+            <Link to="/estamos" className="nav-link">Estamos</Link>
+            <Link to="/contacto" className="nav-link">Contacto</Link>
+            <Link to="/carrito" className="nav-link">
+              Carrito
+              {
+                cart.length > 0 &&
+                <Badge variant="light">{cart.length}</Badge>
+              }
+            </Link>
+          </Nav>
+          <Search setShowNav={setShowNav}/>
+        </Navbar.Collapse>
+        <MenuIcon onClick={() => setShowNav(true)}/>
+        <SideNav 
+          openFromRight={true}
+          showNav={showNav} 
+          onHideNav={() => setShowNav(false)}
+          title={<Logo />}
+          titleStyle={{ backgroundColor: '#000' }}
+          items={navItems}
+        />
+      </Navbar>
+      {/* <Navbar ref={ref} bg="dark" variant="dark" expand="lg">
         <Link to="/" className="navbar-brand">
           <Logo/>
         </Link>
@@ -51,12 +118,9 @@ export default () => {
               }
             </Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="¿Qué estas buscando?" className="mr-sm-2" />
-            <Button variant="outline-success">Buscar</Button>
-          </Form>
+          <Search />
         </Navbar.Collapse>
-      </Navbar>
+      </Navbar> */}
     </NavStyle>
   )
 }
